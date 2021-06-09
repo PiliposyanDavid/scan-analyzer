3b620a1abbfbcde776e8a87e3b04cec2db95dfab
David <piliposyan.david99@gmail.com>
2021-06-09 12:38:23 +0400

    add small change

--- services/analyzer_service.py
+++ services/analyzer_service.py
@@ -2,6 +2,8 @@ import os
 import json
 from PIL import ImageFont, ImageDraw, Image
 from tqdm import tqdm
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import io
